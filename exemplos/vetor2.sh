941ab280c219cb222e0c9276e3c9278c158c9913
RenanPando <55910055+RenanPando@users.noreply.github.com>
2021-06-11 01:49:28 -0300

    Update vetor2.sh

--- exemplos/vetor2.sh
+++ exemplos/vetor2.sh
@@ -1,1 +1,10 @@
+# Autor: Renan Pando de Melo Machado
+# Segundo exercício utilizando vetor: Assim como no script anterior (vetor1.sh), utilizamos um loop pra mostrar os nomes que estão dentro do vetor. Desta vez, estamos usando o while loop.
 
+#!/bin/bash
+
+nomes=("Pedro" "Gabriela" "Mateus")
+for x in ${nomes[@]}
+do
+        echo "Oi, eu sou $x!"
+done
